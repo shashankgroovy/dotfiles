227a759e332aafef0697401c3787132a56111030
Shashank Srivastav <shashankgrovy@gmail.com>
2023-07-09 15:04:57 +0530

    Adds polybar config

---
+++ polybar/launch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Kill already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar, using default config location ~/.config/polybar/config or
+# /etc/polybar/config.ini
+polybar default &
+
