8b481da40d9b5567faf751d97da672ee29f64ec4
Shashank Srivastav <shashankgrovy@gmail.com>
2026-08-06 13:40:09 +0200

    Add CSVview.nvim

--- common/nvim/.config/nvim/lua/plugins/tools.lua
+++ common/nvim/.config/nvim/lua/plugins/tools.lua
@@ -20,4 +20,22 @@ return {
     dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
     opts = {},
   },
+
+  -- Aligned CSV columns with field textobjects/navigation
+  {
+    "hat0uma/csvview.nvim",
+    cmd = { "CsvViewEnable", "CsvViewDisable", "CsvViewToggle" },
+    keys = { { "<leader>tv", "<cmd>CsvViewToggle<cr>", desc = "CSV view" } },
+    opts = {
+      parser = { comments = { "#", "//" } },
+      keymaps = {
+        textobject_field_inner = { "if", mode = { "o", "x" } },
+        textobject_field_outer = { "af", mode = { "o", "x" } },
+        jump_next_field_end = { "<Tab>", mode = { "n", "v" } },
+        jump_prev_field_end = { "<S-Tab>", mode = { "n", "v" } },
+        jump_next_row = { "<Enter>", mode = { "n", "v" } },
+        jump_prev_row = { "<S-Enter>", mode = { "n", "v" } },
+      },
+    },
+  },
 }
